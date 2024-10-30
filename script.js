a69b6304be92fdb01b8a693d53f718db821c5e01
vpkaushik11 <vpkaushik11@gmail.com>
2024-10-30 14:45:52 +0400

    Adding files 30/10

---
+++ script.js
@@ -0,0 +1,38 @@
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const video = document.getElementById("video");
+const gallery = document.getElementById("gallery");
+
+let recorder, stream;
+
+async function startRecording() {
+  stream = await navigator.mediaDevices.getDisplayMedia({
+    video: { mediaSource: "screen" },
+  });
+  recorder = new MediaRecorder(stream);
+
+  const chunks = [];
+  recorder.ondataavailable = (e) => chunks.push(e.data);
+  recorder.onstop = (e) => {
+    const completeBlob = new Blob(chunks, { type: chunks[0].type });
+    video.src = URL.createObjectURL(completeBlob);
+  };
+
+  recorder.start();
+}
+
+start.addEventListener("click", () => {
+  start.setAttribute("disabled", true);
+  stop.removeAttribute("disabled");
+
+  startRecording();
+});
+
+stop.addEventListener("click", () => {
+  stop.setAttribute("disabled", true);
+  start.removeAttribute("disabled");
+
+  recorder.stop();
+  stream.getVideoTracks()[0].stop();
+  document.createElement(<video controls ></video>);
+});
